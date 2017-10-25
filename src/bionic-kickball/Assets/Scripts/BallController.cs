4b25dd49372d8731cc8340fad92506d0f496e8f9
Justin Mancusi <justin.mancusi@gmail.com>
2017-10-25 12:20:00 -0400

    Changed Ball Sprite, Added Kicking Functionality
    TODO: Create 2 Players w/ different controls
    
    Only allow kicking when the ball is very close to the player

---
+++ src/bionic-kickball/Assets/Scripts/BallController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+	public GameObject ball;
+	private bool grounded;
+	private CircleCollider2D _circleCollider;
+	private float distToGround;
+	// Use this for initialization
+	void Awake() {
+		_circleCollider = this.GetComponent<CircleCollider2D>();
+		distToGround = _circleCollider.bounds.extents.y;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public bool isGrounded() {
+		Debug.Log(Physics.Raycast(transform.position, Vector3.down, distToGround + 0.01f));
+		return true;
+	}
+}
