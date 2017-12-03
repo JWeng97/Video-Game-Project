c593c9c84e9f8754f8fef0fdd68b114c92a6eff1
Wengsta123 <jeremy.weng@gmail.com>
2017-12-03 17:01:43 -0500

    Fixed Fading

--- src/bionic-kickball/Assets/Scripts/GameManager.cs
+++ src/bionic-kickball/Assets/Scripts/GameManager.cs
@@ -6,12 +6,6 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour {
 
     public static GameManager instance = null;
-    public Texture2D fadeOutTexture;
-    public float fadeSpeed;
-
-    private int drawDepth = -1000;
-    private float alpha = 1.0f; 
-    private int fadeDir = -1;          //-1 = fade in, 1 = fade out
 
     public int numberOfRounds = 5;
     public int roundsPlayed = 0;
@@ -20,30 +14,10 @@ public class GameManager : MonoBehaviour {
     public int p2Score = 0;
     public AudioClip[] songs;
     private AudioSource audioSource;
-    private void OnGUI()
-    {
-        alpha += fadeDir * fadeSpeed * Time.deltaTime;
-        //force/clamp value to zero and one
-        alpha = Mathf.Clamp01(alpha);
-        //set color of GUI. Colors should ideally remain the same, and only alpha is changed
-        GUI.color = new Color(GUI.color.r, GUI.color.g, GUI.color.b,alpha);
-        GUI.depth = drawDepth; //render on Top
-        GUI.DrawTexture ( new Rect(0, 0, Screen.width, Screen.height),fadeOutTexture); //Texture fits entire screen
-
-    }
+    
 
 
-    public float BeginFade ( int direction)
-    {
-        fadeDir = direction;
-        return (fadeSpeed);
-    }
-
-    void OnLevelWasLoaded()
-    {
-        //alpha = 1
-        BeginFade(-1);
-    }
+   
 
     // Use this for initializatio
     void Awake () {
@@ -54,9 +28,7 @@ public class GameManager : MonoBehaviour {
             Destroy(gameObject);
         DontDestroyOnLoad(gameObject);
 
-        fadeOutTexture = new Texture2D(1,1, TextureFormat.ARGB32, false);
-        fadeOutTexture.SetPixel(0, 0, Color.black);
-        fadeOutTexture.Apply();
+        
 
         audioSource = this.GetComponent<AudioSource>();
 	}
@@ -68,6 +40,7 @@ public class GameManager : MonoBehaviour {
             audioSource.Play();
         } else {
         SceneManager.LoadScene(Random.Range(2, 11));
+            FadeManager.instance.BeginFade(-1);
         if (audioSource.clip != songs[1]) {
             audioSource.clip = songs[1];
             audioSource.Play();
